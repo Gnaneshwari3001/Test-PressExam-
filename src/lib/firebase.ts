88b8ab804d9056eccab202db97dfbaca58739375
Gnaneshwari Sarla <gnaneshwarisarla001@gmail.com>
2025-08-01 06:50:48 +0000

    i have added google also

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth(app);
 
