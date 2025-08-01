4416999aca60d8296a3847cc3d4200075876a679
Gnaneshwari Sarla <gnaneshwarisarla001@gmail.com>
2025-08-01 08:41:16 +0000

    change accordingly and implement continue with google function should wo

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,15 @@ import { getAuth } from "firebase/auth";
 
 // Hardcoded Firebase configuration to ensure it's always available.
 const firebaseConfig = {
-  projectId: "testpress-dyrhv",
-  appId: "1:512988871607:web:5254a3d6ef908bc6224632",
-  storageBucket: "testpress-dyrhv.firebasestorage.app",
-  apiKey: "AIzaSyCtgOjHEdovxj5qctfaMCc6MSA9SiNaq7E",
-  authDomain: "testpress-dyrhv.firebaseapp.com",
-  messagingSenderId: "512988871607",
+    apiKey: "AIzaSyAw0Q7B3F7-r-r55aU8Y5q4j5k9c8l0dY",
+    authDomain: "testpress-68b51.firebaseapp.com",
+    projectId: "testpress-68b51",
+    storageBucket: "testpress-68b51.appspot.com",
+    messagingSenderId: "367069729986",
+    appId: "1:367069729986:web:d71e233b21696e1f0e42d7",
+    measurementId: "G-XXXXXXXXXX"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
